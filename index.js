64518c88aeebdffbd721d1dd178f1e0c0876aa29
Dvorak <longjohnsilver@openmailbox.org>
2017-01-13 11:40:54 +0100

    first version

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-var cool = require("cool-ascii-faces");
 var express = require("express");
 
 var app= express();
-
-app.get('/', fuction(req, res){
-	res.send(cool);
-	console.log('New request');
-
-});
-
 
 app.listen(5003);
