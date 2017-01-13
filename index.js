8b88e0e573e5969c66ad17d0c88c4da57c551703
Dvorak <longjohnsilver@openmailbox.org>
2017-01-13 12:32:01 +0100

    date added

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ var express = require("express");
 
 var app= express();
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 5003);
+
+app.get("/time",(req,res) => {
+
+	var now = new Date();
+	res.write("It is "+now);
+	res.end();
+})
